4b826570c2a80aaa850f217d1692ea2b4e520ee1
SofiGarcia09 <sofigarcia0929@gmail.com>
2024-04-29 18:50:45 -0500

    node js

--- comprodigiosnodejs/server.comprodigios.mjs
+++ comprodigiosnodejs/server.comprodigios.mjs
@@ -3,10 +3,10 @@ import SucursalesController from './features/sucursales/api/v1/sucursales-contro
 const app = express();
 app.use(json());
 
-const sucursalesApiController = new SucursalesController()
-app.use('/api/', sucursalesApiController.getRouter())
+const sucursalesApiController = new SucursalesController();
+app.use('/api/', sucursalesApiController.getRouter());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}` );
-})
+});
